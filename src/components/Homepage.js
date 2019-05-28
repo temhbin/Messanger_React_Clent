9d7780c7d219cf2af67339c80de678aa2758dbb9
temhbin@bk.ru <temhbin@bk.ru>
2019-05-28 09:46:04 -0400

    first commit

---
+++ src/components/Homepage.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import MessageTimeline from "./MessageTimeline";
+
+const Homepage = ({ currentUser }) => {
+  if (!currentUser.isAuthenticated) {
+    return (
+      <div className="home-hero">
+        <h1>Hello</h1>
+        <Link to="/signup" className="btn btn-primary">
+          Sign up here
+        </Link>
+      </div>
+    );
+  }
+  return (
+    <div>
+      <MessageTimeline
+        profileImageUrl={currentUser.user.profileImageUrl}
+        username={currentUser.user.username}
+      />
+    </div>
+  );
+};
+
+export default Homepage;
---
+++ src/containers/About.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "../index.css";
+
+export default () => {
+  return (
+    <div id="about" className="ui container segment">
+      {/*<h3 className="ui header">Big Plant app</h3>*/}
+      {/*<div>*/}
+      {/*  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quis facere*/}
+      {/*  explicabo porro debitis odit, molestiae totam quam delectus adipisci quo*/}
+      {/*  fuga repellat, dignissimos atque vero. Voluptate totam suscipit iusto*/}
+      {/*  commodi!*/}
+      {/*</div>*/}
+    </div>
+  );
+};
